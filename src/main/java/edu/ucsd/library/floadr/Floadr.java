b996adf1d0776747b6b818a7df9b22d91d06eb57
Esmé Cowles <escowles@ucsd.edu>
2014-09-02 12:24:31 -0400

    Working functionality (DAMSKEY-476)

--- src/main/java/edu/ucsd/library/floadr/Floadr.java
+++ src/main/java/edu/ucsd/library/floadr/Floadr.java
@@ -1,5 +1,15 @@
 package edu.ucsd.library.floadr;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.fcrepo.client.FedoraContent;
+import org.fcrepo.client.FedoraDatastream;
+import org.fcrepo.client.FedoraException;
+import org.fcrepo.client.FedoraRepository;
+import org.fcrepo.client.FedoraObject;
+import org.fcrepo.client.impl.FedoraRepositoryImpl;
+
 /**
  * Fedora loader utility.
  * @author escowles
@@ -7,8 +17,18 @@ package edu.ucsd.library.floadr;
 **/
 public class Floadr
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws FedoraException
     {
-        System.out.println( "Hello World" );
+        final String repositoryURL = args[0];
+        final String objectID      = args[1];
+        final FedoraRepository repo = new FedoraRepositoryImpl(repositoryURL);
+        final FedoraObject obj = repo.findOrCreateObject( objectID );
+        System.out.println( "Loaded: " + obj.getName() );
+
+        final InputStream in = new ByteArrayInputStream("test content".getBytes());
+        final FedoraContent content = new FedoraContent().setContent(in).setFilename("tmp.txt")
+            .setContentType("text/plain");
+        final FedoraDatastream ds = repo.createDatastream( objectID + "/content", content );
+        System.out.println( "Created: " + ds.getPath() );
     }
 }
